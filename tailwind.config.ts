ce872143bb0ca754c56c4897ca00facdea593e1c
Dexter <dextersiah@mobius-digital.com>
2024-03-29 07:50:49 +0800

    fix: missing import in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,7 @@
 import type { Config } from 'tailwindcss'
-import colorList from './src/constant/colorList'
 export default <Partial<Config>>{
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,vue}",
     ],
-    theme: {
-        extend: {
-            colors: colorList,
-        }
-    }
 }
