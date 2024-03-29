03591e084c4c8a660b60028ae2978d027ab335fb
Dexter <dextersiah@mobius-digital.com>
2024-03-29 08:05:57 +0800

    chore: rename file/folder

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+
 </script>
 
 <template>
     <main class="h-screen grid place-items-center">
         <div class="flex gap-5">
+            <Button>
+                Default
+            </Button>
         </div>
     </main>
 </template>
