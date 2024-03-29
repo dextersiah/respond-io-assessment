ab17a5f98ccab4077547b80a8652f5b7d1cb54df
Dexter <dextersiah@mobius-digital.com>
2024-03-29 13:57:19 +0800

    feat: added Global Chip component

---
+++ src/components/Global/Chip.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='text-sm rounded py-2 px-4 flex items-center font-bold leading-6' :style="{
+        color: props.foregroundColor,
+        backgroundColor: props.backgroundColor,
+    }">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    foregroundColor: string,
+    backgroundColor: string,
+}>()
+</script>
+
+<style scoped>
+
+</style>
