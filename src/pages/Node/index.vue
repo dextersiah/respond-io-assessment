b8070182271804b48cb574ddb88fa7560d6b4d12
Dexter <dextersiah@mobius-digital.com>
2024-03-30 13:40:59 +0800

    feat: handle sheet transition state

--- src/pages/Node/index.vue
+++ src/pages/Node/index.vue
@@ -6,10 +6,44 @@ import {
     SheetHeader,
     SheetTitle
 } from '@/components/ui/sheet';
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+
+/**
+ * Composables
+ */
+const route = useRoute();
+const router = useRouter()
+
+
+/**
+ * State
+ */
+const sheetState = ref(true);
+
+
+/**
+ * Watchers
+ */
+watch(() => route.query, () => {
+    sheetState.value = true;
+});
+
+watch(sheetState, (value) => {
+    if (!value) {
+        setTimeout(() => {
+            router.push({ name: 'home' });
+        }, 500);
+    }
+});
+
+
+
 </script>
 
 <template>
-    <Sheet :defaultOpen="true">
+    <Sheet v-model:open="sheetState">
         <SheetContent>
             <SheetHeader>
                 <SheetTitle>Are you sure absolutely sure?</SheetTitle>
