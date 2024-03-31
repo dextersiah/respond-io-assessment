23c2ae166418c03a5f55169d3eaeaf463643d48e
Dexter <dextersiah@mobius-digital.com>
2024-03-31 12:24:35 +0800

    update: display payload content for AddCommentNode

--- src/components/VueFlow/Nodes/TriggerNode.vue
+++ src/components/VueFlow/Nodes/TriggerNode.vue
@@ -10,7 +10,12 @@
             </CardTitle>
         </CardHeader>
         <CardContent>
-            Conversation Opened
+            <GlobalText 
+                tag="p" v-for="payload in textPayload"
+                :key="payload.text" 
+            >
+                {{ payload.text }}
+            </GlobalText>
         </CardContent>
         <Handle :id="props.id" type="source" :position="Position.Bottom" />
         <Handle :id="props.id" type="target" :position="Position.Top" />
@@ -19,17 +24,23 @@
 </template>
 
 <script setup lang="ts">
+import GlobalText from '@/components/Global/Text.vue';
 import {
     Card,
     CardContent,
     CardHeader,
     CardTitle
 } from '@/components/ui/card';
-
+import type { NodeData } from '@/types/NodeData';
 import { Handle, Position, type NodeProps } from '@vue-flow/core';
 import { Zap } from 'lucide-vue-next';
+import { computed } from 'vue';
 
-const props = defineProps<NodeProps>()
+const props = defineProps<NodeProps<NodeData>>()
+
+const textPayload = computed(() => {
+    return props?.data?.payload?.filter((item) => item.type === 'text')
+})
  
 </script>
 
